f3fa8f65b438f80b39684763ca5bd838ece010e1
DenysChupryna <anytechdocs@gmail.com>
2017-01-23 22:27:39 +0200

    Исправил 1,3,7

--- src/com/company/hT7.java
+++ src/com/company/hT7.java
@@ -5,34 +5,35 @@ import java.util.Scanner;
 import static java.lang.Math.sqrt;
 
 public class hT7 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("a = ");
-        int a = scanner.nextInt();
+        double a = scanner.nextDouble();
         System.out.println("b = ");
-        int b = scanner.nextInt();
+        double b = scanner.nextDouble();
         System.out.println("c = ");
-        int c = scanner.nextInt();
-        int d = (b*b)-(4*a*c);
+        double c = scanner.nextDouble();
+        double d = (b * b) - (4 * a * c);
         System.out.println("D = " + d);
 
         if (d > 0) {
-            double x1 = (-b+sqrt(d)/2*a);
-            double x2 = (-b-sqrt(d)/2*a);
-            System.out.println("Корни уравнения " + x1 + " " + x2);}
+            double x1 = (-b + sqrt(d) / 2 * a);
+            double x2 = (-b - sqrt(d) / 2 * a);
+            System.out.println("Корни уравнения " + x1 + " и " + x2);
 
-            if (d < 0){
-                System.out.println("Корней нет!");}
-                if (d == 0){
-                    double x3 = (-b)/2*a;
-                    System.out.println("Корень уравнения " + x3);
-                }
+        } else if (d == 0) {
+            double x3 = (-b) / 2 * a;
+            System.out.println("Корень уравнения " + x3);
 
-            }
-
-
+        } else {
+            System.out.println("Корней нет!");
         }
+    }
+}
+
+
+
 
 
 
